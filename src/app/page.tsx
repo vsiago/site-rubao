3d761eee5766c4d2ea4949a0c7e394093c47f68f
vsiago <vsiago22@gmail.com>
2024-04-11 13:51:02 -0300

    melhorias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-"use client"
+"use client";
 import { Metadata } from "next";
 import Image from "next/image";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
-import { useInView } from 'react-intersection-observer';
-
-
+import { useInView } from "react-intersection-observer";
 
 export default function Home() {
   const { ref, inView } = useInView({
@@ -13,40 +11,77 @@ export default function Home() {
   });
 
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="min-h-screen bg-[#D8DBE3] flex flex-col">
       <Header />
-      <section className="flex-1 bg-accent flex items-center md:items-start md:pt-40">
-        <div ref={ref} className={`flex items-center justify-center flex-col gap-6 md:flex-row-reverse md:container ${inView ? 'animate' : ''}`}>
-          <div className="w-full mx-auto">
-            <Image
-              src="/intro-manutencao.png"
-              width={170}
-              height={20}
-              alt="Logo Rubao"
-              className="w-full px-5 mx-auto max-w-[80%] md:hidden"
-            />
-            <Image
-              src="/site-manutencao-bg.svg"
-              width={170}
-              height={20}
-              alt="Logo Rubao"
-              className="hidden pointer-events-none md:flex absolute top-50% right-0 w-[40%] transform -translate-y-1/2"
-            />
-          </div>
-          <div className="text-center md:text-start ">
-            <span className="uppercase text-lg font-semibold tracking-[4px] text-sky-400">
-              Em breve:
-            </span>
-            <h1 className="text-5xl font-bold text-center md:text-start">
-              Novo site!
-            </h1>
-            <p className={`text-center text-lg mx-10 text-slate-400 mt-8 mb-16 md:text-start md:text-2xl md:mx-0 md:pr-40 ${inView ? 'animate animate-delay' : ''}`}>
-              Estamos trabalhando para trazer um novo site, repleto de
-              informações. Fiquem atentos para mais atualizações em breve!
+      <section className=" h-[calc(100vh-150px)] w-full flex bg-gradient-to-br from-[#0E264A] via-[#0E3560] to-[#105F98] items-center md:items-start md:pt-40 ">
+        <div
+          ref={ref}
+          className={`flex items-center w-full justify-center flex-col gap-6 md:flex-row-reverse md:container ${
+            inView ? "animate" : ""
+          }`}
+        >
+
+
+
+
+
+
+
+
+
+
+          <main className="px-8 w-full mt-32 mx-auto">
+            <div className="flex items-baseline gap-3">
+              <span className="text-[#0CC6F5] text-4xl font-semibold italic ml-[24px]">
+                Prefeito
+              </span>
+              <div className="border-2 border-[#0CC6F5] w-16 h-16 rounded-md"></div>
+            </div>
+            <p className="text-[#ffffff] text-8xl font-black italic">Rubão</p>
+            <p className="text-[#98B2C0] text-2xl italic leading-7">
+              Fiz e vou fazer <br></br>muito mais!
             </p>
-          </div>
+
+            <a className="p-3 px-9 w-full border-2 border-[#0CC6F5] rounded-full font-bold text-base text-center gap-3 mt-16 flex items-center justify-center" href="#">
+              <span className="bg-sky-500 h-6 w-6 rounded-sm block rotate-45"></span>
+              <span>
+              Linha do tempo
+              </span>
+            </a>
+
+          </main>
+
+
+
+
+
+
+
+
+
+
         </div>
       </section>
+
+      <div className="custom-shape-divider-bottom-1712845043">
+        <main className="h-[150px] text-center bg-white flex items-center justify-center pb-5">
+          <p className="text-lg text-slate-600 leading-5 rotate-180">
+            Nunca foi Sorte, <br></br>sempre foi Deus!
+          </p>
+        </main>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            className="shape-fill"
+          ></path>
+        </svg>
+      </div>
+
       <Footer />
     </main>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,98 +1,135 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-    const [open, setOpen] = useState(false);
-    const [scrolled, setScrolled] = useState(false);
-    const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
-    const menuItems = [
-        {
-            nome: "Início",
-            link: "/"
-        },
-        {
-            nome: "Biografia do Prefeito",
-            link: "/biografia-prefeito"
-        },
-        {
-            nome: "Agenda e Eventos",
-            link: "/agenda-e-eventos"
-        },
+  const menuItems = [
+    {
+      nome: "Início",
+      link: "/",
+    },
+    {
+      nome: "Biografia do Prefeito",
+      link: "/biografia-prefeito",
+    },
+    {
+      nome: "Agenda e Eventos",
+      link: "/agenda-e-eventos",
+    },
+  ];
 
-    ];
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isScrolled = currentScrollPos > 0;
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollPos = window.pageYOffset;
-            const isScrolled = currentScrollPos > 0;
+      setScrolled(isScrolled);
 
-            setScrolled(isScrolled);
+      if (currentScrollPos > prevScrollPos) {
+        // Scrolling down
+        setPrevScrollPos(currentScrollPos);
+        setOpen(false);
+      } else {
+        // Scrolling up
+        setPrevScrollPos(currentScrollPos);
+      }
+    };
 
-            if (currentScrollPos > prevScrollPos) {
-                // Scrolling down
-                setPrevScrollPos(currentScrollPos);
-                setOpen(false);
-            } else {
-                // Scrolling up
-                setPrevScrollPos(currentScrollPos);
-            }
-        };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, [prevScrollPos]);
-
-    return (
-        <header className={`${scrolled ? "bg-sky-500 fixed top-0 z-10 h-16 flex w-full translate-y-0 duration-300 ease-in-out bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90" : "h-24 bg-accent"} `}>
-            <nav className="min-h-full px-6 flex w-full justify-between items-center md:container mx-auto ">
-            <a href="/">
-                    <Image
-                        src="/rubao-logo-clara.png"
-                        width={150}
-                        height={20}
-                        alt="Logo Rubao"
-                    />
-                </a>
-                <ul
-                    onClick={() => setOpen(!open)}
-                    className="md:hidden relative flex h-10 w-10 flex-col items-center justify-center gap-3 cursor-pointer hover:bg-slate-700 rounded transition duration-150 ease-in hover:transform hover:scale-105"
-                >
-                    <li
-                        className={`${open ? "transform rotate-45 absolute" : ""} w-7 h-[3px] rounded-[2px] bg-white transition duration-150 ease-in-out`}
-                    ></li>
-                    <li
-                        className={`${open ? "transform -rotate-45 absolute" : ""} w-7 h-[3px] rounded-[2px] bg-white transition duration ease-in-out`}
-                    ></li>
-                </ul>
-                <ul
-                    className={`${open ? "bg-accent flex opacity-100 w-full h-screen left-0" : "w-full h-0"} fixed ${scrolled ? "top-16" : "top-20"} right-0 flex flex-col transition duration-200 ease-out opacity-0 flex-1`}
-                >
-                    {menuItems.map((item, index) => (
-                        <li
-                            key={item.nome}
-                            style={{ transitionDelay: `${open ? index * 70 : 0}ms` }}
-                            className={`px-6 ${open ? 'opacity-100 transform translate-x-0 ' : 'opacity-0 transform -translate-x-10'} transition-all ease-out duration-500`}
-                        >
-                            <a
-                                className={`${open ? 'inline-block ml-0 pointer-events-auto transition ease-in py-6' : 'py-0 opacity-0 ml-6 hidden pointer-events-none'} text-slate-300 border-b-[1px] border-slate-700 w-full focus:text-white focus:font-semibold`}
-                                href={`${item.link}`}
-                            >
-                                {item.nome}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-                <ul className="hidden md:flex gap-3">{menuItems.map((item, intex) => (
-                    <li key={item.nome}>
-                        <Link className={`${scrolled ? 'text-slate-700' : ''} h-20 px-2 py-1 text-slate-400 active:text-sky-400`} href={item.link}>{item.nome}</Link>
-                    </li>
-                ))}</ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header
+      className={`${
+        scrolled
+          ? "bg-sky-500 fixed top-0 z-10  flex w-full translate-y-0 duration-300 ease-in-out bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90"
+          : ""
+      }absolute h-24 w-full`}
+    >
+      <nav className="min-h-full px-6 flex w-full justify-between items-center md:container mx-auto ">
+        <a href="/">
+          <Image
+            src="/rubao-logo-clara.svg"
+            width={140}
+            height={20}
+            alt="Logo Rubao"
+          />
+        </a>
+        <ul
+          onClick={() => setOpen(!open)}
+          className="md:hidden relative flex h-10 w-10 flex-col items-center justify-center gap-[8px] cursor-pointer hover:bg-slate-950/40 rounded transition duration-150 ease-in hover:transform hover:scale-105"
+        >
+          <li
+            className={`${
+              open ? "transform rotate-45 absolute w-7" : "w-5"
+            }  h-[3px] rounded-[2px] bg-[#0DB2EE] transition duration-150 ease-in-out`}
+          ></li>
+          <li
+            className={`${
+              open ? "transform -rotate-45 absolute" : ""
+            } w-7 h-[3px] rounded-[2px] bg-white transition duration ease-in-out`}
+          ></li>
+          <li
+            className={`${
+              open ? "transform -rotate-45 absolute w-3" : "w-5"
+            }  h-[3px] rounded-[2px] bg-[#0CE7D5] transition duration ease-in-out`}
+          ></li>
+        </ul>
+        <ul
+          className={`${
+            open
+              ? "bg-gradient-to-br from-[#0E264A] via-[#0E3560] to-[#105F98] flex opacity-100 w-full h-screen left-0 absolute z-[1000]"
+              : "w-full h-0"
+          } fixed ${
+            scrolled ? "top-16" : "top-20"
+          } right-0 flex flex-col transition duration-200 ease-out opacity-0 flex-1`}
+        >
+          {menuItems.map((item, index) => (
+            <li
+              key={item.nome}
+              style={{ transitionDelay: `${open ? index * 70 : 0}ms` }}
+              className={`px-6 ${
+                open
+                  ? "opacity-100 transform translate-x-0 "
+                  : "opacity-0 transform -translate-x-10"
+              } transition-all ease-out duration-500`}
+            >
+              <a
+                className={`${
+                  open
+                    ? "inline-block ml-0 pointer-events-auto transition ease-in py-6"
+                    : "py-0 opacity-0 ml-6 hidden pointer-events-none"
+                } text-slate-300 border-b-[1px] border-slate-950/40 w-full focus:text-white focus:font-semibold`}
+                href={`${item.link}`}
+              >
+                {item.nome}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul className="hidden md:flex gap-3">
+          {menuItems.map((item, intex) => (
+            <li key={item.nome}>
+              <Link
+                className={`${
+                  scrolled ? "text-slate-700" : ""
+                } h-20 px-2 py-1 text-slate-400 active:text-sky-400`}
+                href={item.link}
+              >
+                {item.nome}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
