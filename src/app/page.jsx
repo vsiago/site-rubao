46d5d2a9e52859fd4bec6fb8829dd300aa2ef24f
vsiago <vsiago22@gmail.com>
2024-08-08 12:06:44 -0300

    voltando

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,25 +32,28 @@ export default function Home() {
   };
 
   return (
-    <main className="min-h-[900px] bg-[#D8DBE3] flex flex-col overflow-x-clip">
+    <>
       <Header />
-      {/* <Image
+      <Image
         width={55}
         height={55}
-        className="absolute -right-[150px] top-[100px] w-full opacity-10"
+        className="absolute w-[50%] -right-[150px] top-[100px] opacity-5"
         alt="logo B Bubão"
         src={require("../../public/logo-b.svg")}
-      /> */}
-      <section className=" h-screen w-full flex bg-gradient-to-br from-[#0E264A] via-[#0E3560] to-[#105F98] items-center md:items-start md:pt-40 ">
+      />
+
+
+      {/* ********************* SECTION HERO ********************* */}
+      <section className="flex-1 min-h-screen w-full flex flex-col bg-gradient-to-br from-[#0E264A] via-[#0E3560] to-[#105F98] items-start justify-start md:items-start md:pt-40">
         <div
           ref={ref}
-          className={`flex items-center w-full justify-center flex-col gap-6 md:flex-row-reverse md:container ${
+          className={`flex items-center w-full justify-center flex-col md:container flex-1 ${
             inView ? "" : ""
           }`}
         >
-          <main className="px-8 w-full mt-36 mx-auto">
+          <main className="w-full">
             <div className="flex items-baseline gap-3">
-              <span className="animate text-[#0CC6F5] text-4xl font-semibold italic ml-[15px] -mr-2">
+              <span className="animate text-[#0CC6F5] text-4xl font-semibold italic ">
                 Prefeito
               </span>
               <div className="">
@@ -63,15 +66,15 @@ export default function Home() {
                 />
               </div>
             </div>
-            <p className="animate delay1 text-[#ffffff] text-8xl font-black italic">
+            <p className="animate delay1 text-[#ffffff] text-8xl font-black ">
               Rubão
             </p>
-            <p className="animate delay2 text-[#98B2C0] text-2xl italic leading-7">
-              Fiz e vou fazer <br></br>muito mais!
+            <p className="animate delay2 text-[#98B2C0] text-2xl  leading-7">
+              Fiz e vou fazer muito mais!
             </p>
 
             <a
-              className="p-3 px-9 w-full border-2 border-[#0CC6F5] rounded-full font-bold text-base text-center gap-3 mt-16 flex items-center justify-center"
+              className="p-3 px-9 w-full md:w-[30%] border-2 border-[#0CC6F5] rounded-full font-bold text-base text-center gap-3 mt-16 flex items-center justify-center cursor-pointer"
               onClick={() =>
                 scroller.scrollTo("linha-do-tempo", {
                   smooth: true,
@@ -87,19 +90,22 @@ export default function Home() {
                   src={require("../../public/arrow.svg")}
                 />
               </div>
-              <span>Linha do tempo</span>
+              <span className="text-white text-xl">Linha do tempo</span>
             </a>
           </main>
         </div>
-      </section>
-
-      <div className="custom-shape-divider-bottom-1712845043">
-        <main className="h-[100px] text-center bg-white flex items-center justify-center ">
-          <p className="text-lg text-slate-600 leading-5 rotate-180">
+        <main className="h-[250px] text-center bg-gradient-to-t from-white to-white/0 flex w-full py-10">
+              <div className="container mx-auto flex items-end justify-end">
+              <p className="text-2xl text-slate-600 text-end">
             Nunca foi Sorte, <br></br>sempre foi Deus!
           </p>
+              </div>
         </main>
-        <svg
+      </section>
+
+
+
+        {/* <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
@@ -109,12 +115,10 @@ export default function Home() {
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
             className="shape-fill"
           ></path>
-        </svg>
-      </div>
-
+        </svg> */}
       <section
         id="linha-do-tempo"
-        className="min-h-screen bg-[#0E264A] p-7 py-14"
+        className="min-h-screen bg-white p-7 py-14"
       >
         <p className="text-bold text-xl mt-10 text-[#799EBF]">Linha do tempo</p>
 
@@ -146,8 +150,8 @@ export default function Home() {
                     src={require("../../public/arrow.svg")}
                     className={`${
                       modalActiveIndex === index
-                        ? "rotate-180 -translate-y-1"
-                        : "rotate-0 translate-y-1"
+                        ? " "
+                        : ""
                     }  transition-all 1s ease-in-out`}
                   />
                 </div>
@@ -173,7 +177,7 @@ export default function Home() {
         </nav>
       </section>
       <Footer />
-    </main>
+    </>
   );
 }
 // Tell Next.js to remount this component on every edit
