8555861dcadce21dba94e95eba0025953e5bb734
vsiago <vsiago22@gmail.com>
2024-08-19 13:31:24 -0300

    melhora botao

--- src/app/theme/page.jsx
+++ src/app/theme/page.jsx
@@ -1,11 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ImageCropper from "../../components/ImageCropper"; // Ajuste o caminho conforme necessário
 
 const Theme = () => {
   const [imageSrc, setImageSrc] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleFileClick = () => {
+    fileInputRef.current.click();
+  };
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -22,7 +27,19 @@ const Theme = () => {
     <main className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1 flex flex-col items-center justify-center p-10">
-        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <div
+          onClick={handleFileClick}
+          className="w-64 h-12 flex items-center justify-center bg-blue-500 text-white rounded cursor-pointer"
+        >
+          Selecione uma imagem
+        </div>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          style={{ display: 'none' }}
+          ref={fileInputRef}
+        />
         {imageSrc && <ImageCropper imageSrc={imageSrc} />}
       </main>
       <Footer />
