c1014a0aca2eeabcf3a3d0cc459b833b04a1b21f
vsiagoCodeCity <iago@codecity.app>
2024-08-25 11:39:44 -0300

    perfeito

--- src/app/temas/profile/page.jsx
+++ src/app/temas/profile/page.jsx
@@ -4,9 +4,11 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ImageCropper from "../../../components/ImageCropper"; // Ajuste o caminho conforme necessário
 import Image from "next/image";
+import { FiCopy } from "react-icons/fi"; // Importar ícone de copiar
 
 const Profile = () => {
   const [imageSrc, setImageSrc] = useState(null);
+  const [copySuccess, setCopySuccess] = useState(false); // Estado para a mensagem de confirmação
   const fileInputRef = useRef(null);
 
   const handleFileClick = () => {
@@ -24,13 +26,23 @@ const Profile = () => {
     }
   };
 
+  const handleCopyClick = () => {
+    navigator.clipboard
+      .writeText(window.location.href)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => setCopySuccess(false), 2000); // Ocultar mensagem após 2 segundos
+      })
+      .catch((err) => console.error("Failed to copy: ", err));
+  };
+
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-b from-[#053C81]/90 to-[#003055]">
       <Header />
       <main className="flex-1 flex flex-col items-center justify-center p-10 mb-20 mt-12">
         <div className="my-7">
           {!imageSrc && (
-            <div className="h-64 w-64   mx-auto">
+            <div className="relative h-64 w-64 mx-auto">
               <Image
                 width={300}
                 height={300}
@@ -38,16 +50,27 @@ const Profile = () => {
                 src="/example-profile.png"
                 className="w-full h-full object-contain"
               />
+              <button
+                onClick={handleCopyClick}
+                className="absolute top-0 -right-6 p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600"
+              >
+                <FiCopy size={28} />
+              </button>
+              {copySuccess && (
+                <span className="absolute top-12 right-0 p-2 bg-green-500 text-white rounded-md shadow-lg">
+                  Link copiado!
+                </span>
+              )}
             </div>
           )}
-          <p className="text-3xl md:text-4xl  font-semibold text-center">
+          <p className="text-3xl md:text-4xl font-semibold text-center">
             Personalize sua <br className="md:hidden" />
             foto de perfil
           </p>
-          <p className="text-center text-xs md:text-base font-light text-slate-400 mt-2">
+          {/* <p className="text-center text-xs md:text-base font-light text-slate-400 mt-2">
             Recomendamos uma imagem <br className="md:hidden" /> quadrada 4:4
             para melhor encaixe.
-          </p>
+          </p> */}
         </div>
 
         {/* Esconde o botão quando imageSrc está ativo */}
