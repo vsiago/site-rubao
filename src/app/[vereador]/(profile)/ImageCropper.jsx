58062404c470d2a2f16276bab889e92a5cc6baa0
vsiagoCodeCity <iago@codecity.app>
2024-08-25 11:00:13 -0300

    código vereadores unificado em 1

--- src/app/[vereador]/(profile)/ImageCropper.jsx
+++ src/app/[vereador]/(profile)/ImageCropper.jsx
@@ -1,7 +1,58 @@
-// ImageCropper.js
 import React, { useState, useCallback, useEffect } from "react";
 import Cropper from "react-easy-crop";
-import getCroppedImg from "./getCroppedImg"; // Ajuste o caminho conforme a localização real do arquivo
+
+const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = (error) => reject(error);
+    img.src = url;
+  });
+
+const getCroppedImg = async (imageSrc, crop, overlayImageUrl) => {
+  try {
+    // Carrega a imagem do usuário
+    const userImage = await createImage(imageSrc);
+
+    // Carrega a imagem de overlay
+    const overlayImage = await createImage(overlayImageUrl); // Ajuste o caminho conforme necessário
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // Define as dimensões do canvas para o tamanho do crop
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+
+    // Desenha a imagem recortada no canvas
+    ctx.drawImage(
+      userImage,
+      crop.x,
+      crop.y,
+      crop.width,
+      crop.height,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    // Adiciona o overlay se a imagem de overlay estiver carregada
+    if (overlayImage) {
+      ctx.drawImage(overlayImage, 0, 0, crop.width, crop.height);
+    }
+
+    // Converte o canvas para uma URL de objeto (Blob URL)
+    return new Promise((resolve) => {
+      canvas.toBlob((blob) => {
+        resolve(URL.createObjectURL(blob));
+      }, "image/png");
+    });
+  } catch (error) {
+    console.error("Erro ao recortar imagem:", error);
+    throw error;
+  }
+};
 
 const ImageCropper = ({ imageSrc, downloadFileName, backgroundImageUrl }) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
