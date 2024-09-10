7416e560fa36c93478099c7902cd12810c8c8cb2
vsiago <vsiago22@gmail.com>
2024-09-10 10:58:26 -0300

    localizando a imagem com geolocalização

--- src/components/ARComponent.js
+++ src/components/ARComponent.js
@@ -1,46 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
-const ARComponent = () => {
-  const [canSeeAR, setCanSeeAR] = useState(false);
-
+const GeoARComponent = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // Importa 'aframe' e 'mind-ar' somente no cliente
       require('aframe');
-      require('mind-ar/dist/mindar-image-aframe.prod.js');
-
-      // Ativa AR diretamente para teste
-      setCanSeeAR(true);
+      require('ar.js/aframe/build/aframe-ar.js');
     }
   }, []);
 
   return (
     <div>
-      {canSeeAR ? (
-        <a-scene
-          mindar="imageTargetSrc: /assets/target.mind; autoStart: true;"
-          embedded
-          vr-mode-ui="enabled: false"
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        style={{ width: '100%', height: '100vh' }}
+        gps-camera
+      >
+        {/* Define o objeto com base em lat/long */}
+        <a-entity
+          gps-entity-place="latitude: -22.8669; longitude: -43.7751"
+          geometry="primitive: box"
+          material="color: red;"
+          scale="10 10 10"
         >
-          <a-camera
-            position="0 0 0"
-            look-controls="enabled: true"
-            mindar-camera="videoConstraints: { facingMode: { exact: 'environment' } }"
-            // Usa a câmera traseira
-          ></a-camera>
+          <a-text value="Prefeitura de Itaguaí" scale="50 50 50" position="0 50 0"></a-text>
+        </a-entity>
 
-          {/* Exemplo de texto "Norte" movido para o topo */}
-          <a-entity position="0 2 -5">
-            <a-text value="Norte" align="center" color="red"></a-text>
-          </a-entity>
-        </a-scene>
-      ) : (
-        <p>Ative a realidade aumentada mirando para o céu.</p>
-      )}
+        {/* A câmera com suporte a GPS */}
+        <a-camera gps-camera="simulateAltitude: false; positionMinAccuracy: 100"></a-camera>
+      </a-scene>
     </div>
   );
 };
 
-export default dynamic(() => Promise.resolve(ARComponent), { ssr: false });
+export default dynamic(() => Promise.resolve(GeoARComponent), { ssr: false });
