b0341ae6632343ab104ea76680183d9e6cd77535
vsiago <vsiago22@gmail.com>
2024-09-10 14:56:06 -0300

    atual

--- src/components/ARComponent.js
+++ src/components/ARComponent.js
@@ -1,31 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
-// Função para calcular a distância entre dois pontos geográficos
-const haversineDistance = (lat1, lon1, lat2, lon2) => {
-  const toRad = (value) => (value * Math.PI) / 180;
-  const R = 6371; // Raio da Terra em quilômetros
-
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  return R * c; // Distância em quilômetros
-};
-
 const ARComponent = () => {
   const [userPosition, setUserPosition] = useState(null);
-  const [showImage, setShowImage] = useState(false);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       require('aframe');
-
+      
+      // Obter a posição do usuário
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
           setUserPosition({
@@ -37,24 +20,23 @@ const ARComponent = () => {
     }
   }, []);
 
-  useEffect(() => {
+  // Latitude e Longitude da Prefeitura de Itaguaí
+  const prefeituraLatitude = -22.8641035; // Substitua pela latitude real
+  const prefeituraLongitude = -43.7799832; // Substitua pela longitude real
+
+  // Função para verificar se a posição do usuário está alinhada com a Prefeitura
+  const isPointingToSky = () => {
     if (userPosition) {
-      const distance = haversineDistance(
-        userPosition.latitude,
-        userPosition.longitude,
-        prefeituraLatitude,
-        prefeituraLongitude
+      const distance = Math.sqrt(
+        Math.pow(userPosition.latitude - prefeituraLatitude, 2) +
+        Math.pow(userPosition.longitude - prefeituraLongitude, 2)
       );
 
-      console.log('Distância:', distance); // Adicione este log para verificar a distância
-
-      const shouldShowImage = distance < 0.01;
-      setShowImage(shouldShowImage);
+      // Ajuste o valor de distância para quando a imagem deve aparecer
+      return distance < 0.01; // Ajuste conforme necessário
     }
-  }, [userPosition]);
-
-  const prefeituraLatitude = -22.8641035;
-  const prefeituraLongitude = -43.7799832;
+    return false;
+  };
 
   return (
     <div>
@@ -65,7 +47,8 @@ const ARComponent = () => {
       >
         <a-camera position="0 0 0"></a-camera>
         
-        {showImage && (
+        {/* Mostrar imagem apenas se estiver apontando para o céu */}
+        {isPointingToSky() && (
           <a-image
             src="/ar-rubao20.png"
             position="0 1.5 -3"
