aed0bd203cd72414c964e023581c87f3851b8a36
vsiago <vsiago22@gmail.com>
2024-09-10 11:28:23 -0300

    teste norte

--- src/components/ARComponent.js
+++ src/components/ARComponent.js
@@ -1,40 +1,42 @@
 import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
-const GeoARComponent = () => {
+const ARComponent = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       require('aframe');
-      require('mind-ar/dist/mindar-image-aframe.prod.js');
     }
   }, []);
-
-  // Coordenadas da Prefeitura de Itaguaí
-  const cityHallLat = -22.8669;
-  const cityHallLon = -43.7751;
 
   return (
     <div>
       <a-scene
-        vr-mode-ui="enabled: false"
         embedded
         style={{ width: '100%', height: '100vh' }}
-        gps-camera="simulateAltitude: true;" // Removido positionMinAccuracy para exibir sempre
+        vr-mode-ui="enabled: false"
+        // Adiciona o componente AR
       >
-        {/* O cubo será sempre visível independentemente da distância */}
+        {/* A câmera traseira é utilizada */}
+        <a-camera 
+          position="0 0 0" 
+          look-controls="enabled: false" 
+          // Configura a câmera traseira
+          video="facingMode: environment"
+        ></a-camera>
+
+        {/* Cubo simples apontado para o norte */}
         <a-entity
-          gps-entity-place={`latitude: ${cityHallLat}; longitude: ${cityHallLon}`}
           geometry="primitive: box"
           material="color: red;"
           scale="10 10 10"
+          position="0 0 -5"  // Ajuste a posição para que o cubo seja visível
+          rotation="0 0 0"  // Cubo voltado para o norte
         >
-          <a-text value="Prefeitura de Itaguaí" scale="50 50 50" position="0 50 0"></a-text>
+          <a-text value="Cubo Norte" scale="5 5 5" position="0 0 0.5"></a-text>
         </a-entity>
-
-        <a-camera gps-camera="simulateAltitude: true;"></a-camera>
       </a-scene>
     </div>
   );
 };
 
-export default dynamic(() => Promise.resolve(GeoARComponent), { ssr: false });
+export default dynamic(() => Promise.resolve(ARComponent), { ssr: false });
