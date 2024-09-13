4aa852a2855f82c5b3dbf637544afb869885d071
vsiago <vsiago22@gmail.com>
2024-09-13 09:16:23 -0400

    teste melhoria

--- src/app/rubao-interativo/page.jsx
+++ src/app/rubao-interativo/page.jsx
@@ -1,21 +1,27 @@
-"use client"
-import { useEffect } from 'react';
+"use client";
+import { useEffect, useState } from 'react';
 
 const ARPage = () => {
+  const [scriptsLoaded, setScriptsLoaded] = useState(false);
+
   useEffect(() => {
-    if (!document.querySelector('script[src="https://aframe.io/releases/1.6.0/aframe.min.js"]')) {
+    const loadScripts = () => {
+      // Carregar o script A-Frame
       const scriptAFrame = document.createElement('script');
       scriptAFrame.src = 'https://aframe.io/releases/1.6.0/aframe.min.js';
       scriptAFrame.async = true;
+      scriptAFrame.onload = () => {
+        // Depois de carregar o A-Frame, carregar o MindAR
+        const scriptMindAR = document.createElement('script');
+        scriptMindAR.src = 'https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-aframe.prod.js';
+        scriptMindAR.async = true;
+        scriptMindAR.onload = () => setScriptsLoaded(true); // Marcar scripts como carregados
+        document.body.appendChild(scriptMindAR);
+      };
       document.body.appendChild(scriptAFrame);
-    }
+    };
 
-    if (!document.querySelector('script[src="https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-aframe.prod.js"]')) {
-      const scriptMindAR = document.createElement('script');
-      scriptMindAR.src = 'https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-aframe.prod.js';
-      scriptMindAR.async = true;
-      document.body.appendChild(scriptMindAR);
-    }
+    loadScripts();
 
     return () => {
       const scriptAFrame = document.querySelector('script[src="https://aframe.io/releases/1.6.0/aframe.min.js"]');
@@ -26,24 +32,43 @@ const ARPage = () => {
     };
   }, []);
 
-  // Teste
+  const enterFullscreen = () => {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+      document.documentElement.msRequestFullscreen();
+    }
+  };
+
+  if (!scriptsLoaded) {
+    return <div>Loading...</div>; // Opcional: uma mensagem de carregamento
+  }
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+       <button
+        onClick={enterFullscreen}
+        className='p-3 px-6 bg-sky-500 text-lg font-semibold text-white rounded absolute bottom-2 left-2'
+      >
+        Entrar em tela cheia
+      </button>
       <a-scene
-        mindar-image={`imageTargetSrc: /targets.mind;`} // Atualize para o caminho do seu arquivo targets.mind
+        mindar-image="imageTargetSrc: /targets.mind;" // Atualize para o caminho do seu arquivo targets.mind
         color-space="sRGB"
         renderer="colorManagement: true, physicallyCorrectLights"
         vr-mode-ui="enabled: false"
         device-orientation-permission-ui="enabled: false"
+        style={{ width: '100%', height: '100%' }}
       >
         <a-assets>
-          {/* Carrega a imagem PNG do overlay */}
           <img
             id="overlayImage"
             src="/rubao-santinho-frame.png"
             crossorigin="anonymous"
-            height
           />
         </a-assets>
 
@@ -52,20 +77,17 @@ const ARPage = () => {
           look-controls="enabled: false"
           fullscreen
         ></a-camera>
-        
-        {/* Entidade que será renderizada quando o target for detectado */}
+
         <a-entity mindar-image-target="targetIndex: 0">
-          {/* Plane onde o overlay será aplicado */}
           <a-plane
-            src="#overlayImage"  // Referencia a imagem carregada no <a-assets>
+            src="#overlayImage"
             position="0 0 0"
             height="1.2"
             width="1"
             rotation="0 0 0"
-            material="transparent: true; opacity: 1" 
+            material="transparent: true; opacity: 1"
           ></a-plane>
           
-          {/* Modelo 3D se desejado */}
           <a-gltf-model
             rotation="0 0 0"
             position="0 0 0.1"
@@ -75,6 +97,8 @@ const ARPage = () => {
           ></a-gltf-model>
         </a-entity>
       </a-scene>
+
+     
     </div>
   );
 };
